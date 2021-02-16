062dc35ebac12db9bf3e94619defc2a43a03fed2
grassweiler <gabriel@conectra.net.br>
2021-02-16 16:54:55 -0300

    [ADD] - programa funcionando cap5

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,4 +1,3 @@
-
 class NegociacaoController {
 
     constructor() {
@@ -7,24 +6,32 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
     }
 
     adiciona(event) {
 
         event.preventDefault();
+        this._negociacoes.adiciona(this._criaNegociacao());
+        console.log(this._negociacoes);
+        this._limpaFormulario();
+        
+    }
 
-        let data = new Date(...
-            this._inputData.value
-                .split('-')
-                .map((item, indice) => item - indice % 2)
-        );
+    _limpaFormulario() {
 
-        let negociacao = new Negociacao(
-            data,
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
+        this._inputData.focus();
+    }
+
+    _criaNegociacao() {
+
+        return new Negociacao(
+            DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
-
-        console.log(negociacao);
     }
 }
