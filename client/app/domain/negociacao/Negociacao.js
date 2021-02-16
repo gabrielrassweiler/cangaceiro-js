5af3706aa75f53b62a837eb6ee8505b74e1b3ad0
grassweiler <gabriel@conectra.net.br>
2021-02-16 09:33:37 -0300

    [UPD] #arquivo - nmodel pronto de negociacoes

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,7 +1,29 @@
-class Negociacao{
-    constructor(){
-        this.data = data; //data atual
-        this.quantidade = quantidade;
-        this.valor = valor;
+ 
+class Negociacao {
+
+    constructor(_data, _quantidade, _valor) {
+        Object.assign(this, { _quantidade, _valor })
+        this._data = new Date(_data.getTime());
+        Object.freeze(this);
+    }
+
+    get volume() {
+
+        return this._quantidade * this._valor;
+    }
+
+    get data() {
+
+        return new Date(this._data.getTime());
+    }
+
+    get quantidade() {
+
+        return this._quantidade;
+    }
+
+    get valor() {
+
+        return this._valor;
     }
 }
